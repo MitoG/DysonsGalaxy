eccf22217a2850fa52f16b9c9df54f6271a20826
MitoG <mitog@mitog.io>
2021-02-05 18:06:30 +0100

    version 1.0.4

--- DysonsGalaxy/Properties/AssemblyInfo.cs
+++ DysonsGalaxy/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("DysonsGalaxy")] //Set this to the full name of the mod including spaces.
 [assembly: AssemblyTitle("DysonsGalaxy")] //This is only used when mousing over a dll file in Windows explorer.
 [assembly: AssemblyDescription("A UMF Mod(umodframework.com) for Dyson Sphere Program")]
-[assembly: AssemblyCompany("MitoG")] //Set this to your name/nickname and/or website
+[assembly: AssemblyCompany("")] //Set this to your name/nickname and/or website
 [assembly: AssemblyCopyright("© 2019 MitoG. All rights reserved.")] //Set this to your copyright name.
-[assembly: AssemblyVersion("1.0.3")] //Set this to the version of your mod.
-[assembly: AssemblyFileVersion("1.0.3")] //Set this to the version of your mod.
+[assembly: AssemblyVersion("1.0.4")] //Set this to the version of your mod.
+[assembly: AssemblyFileVersion("1.0.4")] //Set this to the version of your mod.
 
 [assembly: ComVisible(false)]
 [assembly: Guid("1da31cec-1708-45f3-9933-d97e6a0a7ee3")]
